22d4c0f911a1bd4813e5d8423394e94b0e935eaf
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2021-11-14 15:59:53 +0700

    menambahkan utils/cors.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 import config from 'config'
 import cors from 'cors'
+import corsOptions from './utils/cors'
 import logger from './utils/logger'
 import routes from './routes'
 import swagerDocs from './utils/swagger'
@@ -8,13 +9,7 @@ import swagerDocs from './utils/swagger'
 const app = express()
 const port = config.get<number>('port')
 
-// Daftar origin yang diperbolehkan mengakses aplikasi ini dari sisi klien
-const allowedOrigins = ['http://localhost:3000', 'http://www.kompas.local:3000', 'https://www.kompas.id', 'https://preview.kompas.id']
-const corsOptions: cors.CorsOptions = {
-  origin: allowedOrigins
-}
 app.use(cors(corsOptions))
-
 app.use(express.json())
 
 app.listen(port, async () => {
