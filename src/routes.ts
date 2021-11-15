ce026f6992e9ad1ef98548b9e15658abb871167f
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2021-11-15 18:14:41 +0700

    melengkapi dokumentasi rute /api/v1/twitter

--- src/routes.ts
+++ src/routes.ts
@@ -8,7 +8,7 @@ function routes (app: Express) {
    * /:
    *   get:
    *     summary: Rute untuk mengecek kondisi aplikasi
-   *     description: Rute untuk mengecek kondisi aplikasi. Mengembalikan respons berstatus 200 berupa obyek jika aplikasi sedang berjalan.
+   *     description: Rute untuk mengecek kondisi aplikasi, mengembalikan respons berstatus 200 berupa obyek jika aplikasi sedang berjalan.
    *     responses:
    *       200:
    *         description: Oke
@@ -23,6 +23,82 @@ function routes (app: Express) {
    */
   app.get('/', (req: Request, res: Response) => res.json({'ping': 'pong'}))
 
+  /**
+   * @swagger
+   * /api/v1/twitter:
+   *   get:
+   *     summary: Rute untuk mengambil konten oembed Twitter.
+   *     description: Rute untuk mengambil konten oembed Twitter, memerlukan kueri `url` (wajib) dan `omitScript` (manasuka).
+   *     parameters:
+   *       - in: query
+   *         name: url
+   *         description: URL ke cuitan bersangkutan
+   *         required: true
+   *         style: form
+   *         allowEmptyValue: false
+   *         example: https://twitter.com/ixavieruncle/status/1457667985817042951
+   *       - in: query
+   *         name: omitScript
+   *         description: Menentukan apakah tag `<script>` disertakan dalam bodi respons dari Twitter. Nilai default `false`
+   *         required: false
+   *         allowEmptyValue: true
+   *         example: true
+   *     responses:
+   *       200:
+   *         description: Oke
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 url:
+   *                   type: string
+   *                   example: https://twitter.com/ixavieruncle/status/1457667985817042951
+   *                 author_name:
+   *                   type: string
+   *                   example: Uncle
+   *                 author_url:
+   *                   type: string
+   *                   example: https://twitter.com/ixavieruncle
+   *                 html:
+   *                   type: string
+   *                   example: <blockquote class="twitter-tweet"><p lang="en" dir="ltr">Men&#39;s perfume last longer than their promises</p>&mdash; Uncle (@ixavieruncle) <a href="https://twitter.com/ixavieruncle/status/1457667985817042951?ref_src=twsrc%5Etfw">November 8, 2021</a></blockquote>\n
+   *                 width:
+   *                   type: number
+   *                   example: 550
+   *                 height:
+   *                   type: number
+   *                   example: null
+   *                   nullable: true
+   *                 type:
+   *                   type: string
+   *                   example: rich
+   *                 cache_age:
+   *                   type: string
+   *                   example: 3153600000
+   *                 provider_name:
+   *                   type: string
+   *                   example: Twitter
+   *                 provider_url:
+   *                   type: string
+   *                   example: https://twitter.com
+   *                 version:
+   *                   type: string
+   *                   example: 1.0
+   *       404:
+   *         description: Cuitan tak ditemukan
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 status:
+   *                   type: number
+   *                   example: 404
+   *                 message:
+   *                   type: string
+   *                   example: Request failed with status code 404
+   */
   app.get('/api/v1/twitter', twitterFetch)
 }
 
