b5671a675ee5b6826c4673f7488da63774d7265f
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2021-12-01 10:35:11 +0700

    koreksi unit test /api/v1/twitter

--- tests/twitter.test.ts
+++ tests/twitter.test.ts
@@ -1,5 +1,7 @@
 import request from 'supertest'
-import { app } from '../src/app'
+import runServer from '../src/utils/server'
+
+const app = runServer()
 
 describe('/api/v1/twitter', () => {
   describe('given required url and optional omit_script queries are valid', () => {
@@ -32,29 +34,23 @@ describe('/api/v1/twitter', () => {
       expect(response.statusCode).toBe(200)
       expect(response.body.html).not.toContain('<script')
     })
-
-
   })
 
   describe('given required url and optional omit_script queries are invalid', () => {
     it('invalid tweet url provided, it should return 404 status code', async () => {
-      const response = await request(app).get('/api/v1/twitter?url=https://twitter.com/ixavieruncle/status/1368546562335371269a')
-      expect(response.statusCode).toBe(404)
+      await request(app).get('/api/v1/twitter?url=https://twitter.com/ixavieruncle/status/1368546562335371269a').expect(404)
     })
 
     it('invalid random url provided, it should return 400 status code', async () => {
-      const response = await request(app).get('/api/v1/twitter?url=https://www.facebook.com/interestingengineering/videos/1853181661418196')
-      expect(response.statusCode).toBe(400)
+      await request(app).get('/api/v1/twitter?url=https://www.facebook.com/interestingengineering/videos/1853181661418196').expect(400)
     })
 
     it('empty url provided, it should return 400 status code', async () => {
-      const response = await request(app).get('/api/v1/twitter?url=')
-      expect(response.statusCode).toBe(400)
+      await request(app).get('/api/v1/twitter?url=').expect(400)
     })
 
     it('no url provided, it should return 400 status code', async () => {
-      const response = await request(app).get('/api/v1/twitter')
-      expect(response.statusCode).toBe(400)
+      await request(app).get('/api/v1/twitter').expect(400)
     })
 
     it('omit_script value is mbahmu_kiper, it should return 200 status code and body.html contains "<script>" tag', async () => {
